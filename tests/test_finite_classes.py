109a40555e8dd1941e59fc70b1223792c32d7a4f
Abigail Ollson <145692399+Ollson2921@users.noreply.github.com>
2025-08-08 09:36:47 +0100

    Tests for finite classes

--- tests/test_finite_classes.py
+++ tests/test_finite_classes.py
@@ -1,19 +1,19 @@
-# from cperms_ins_enc import HorizontalSearcher, VerticalSearcher
+from cperms_ins_enc import HorizontalSearcher, VerticalSearcher
 
 
-# def test_horizontal_searcher():
-#     """
-#     Test the HorizontalSearcher with a finite class of Cayley permutations.
-#     """
-#     basis = "210, 012, 100"
+def test_horizontal_searcher():
+    """
+    Test the HorizontalSearcher with a finite class of Cayley permutations.
+    """
+    basis = "210, 012, 100"
 
-#     assert HorizontalSearcher(basis).auto_search(max_expansion_time=60000, debug=True)
+    assert HorizontalSearcher(basis).auto_search(max_expansion_time=60000)
 
 
-# def test_vertical_searcher():
-#     """
-#     Test the VerticalSearcher with a finite class of Cayley permutations.
-#     """
-#     basis = "210, 012, 100"
+def test_vertical_searcher():
+    """
+    Test the VerticalSearcher with a finite class of Cayley permutations.
+    """
+    basis = "210, 012, 100"
 
-#     assert VerticalSearcher(basis).auto_search(max_expansion_time=60000, debug=True)
+    assert VerticalSearcher(basis).auto_search(max_expansion_time=60000)
