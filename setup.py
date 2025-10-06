c76cec7f2aae636b3726d7dc521f75665d37d04e
Christian Bean <christianbean93@gmail.com>
2025-10-06 15:08:46 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_namespace_packages
 
 setup(
     name="cperms_ins_enc",
@@ -6,9 +6,7 @@ setup(
     description="A module for enumerating Cayley permutations using the insertion encoding.",
     author="Christian Bean, Abigail Ollson",
     author_email="a.n.ollson@keele.ac.uk",
-    packages=[
-        "insertion_encoding",
-    ],
+    packages=find_namespace_packages(),
     keywords="enumerative combinatorics pattern avoidance cayley permutations insertion encoding",
     install_requires=[
         "comb_spec_searcher",
