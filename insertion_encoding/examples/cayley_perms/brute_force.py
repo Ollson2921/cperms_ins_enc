4ef81684e14c8b320f5d948184c64a11c531b76e
Abigail Ollson <a.n.ollson@keele.ac.uk>
2025-11-04 10:50:19 +0000

    brute force updates

--- insertion_encoding/examples/cayley_perms/brute_force.py
+++ insertion_encoding/examples/cayley_perms/brute_force.py
@@ -9,15 +9,21 @@ basis_patterns = string_to_basis(basis)
 
 # Prints all Cayley permutation of size 3 in the class Av{basis_patterns}
 m = 3
-print(Av(basis_patterns).generate_cperms(m))
+print(f"Cayley permutations length {m} in Av({basis})")
+for cperm in Av(basis_patterns).generate_cperms(m):
+    print(cperm)
+
+print(f"Number of Cayley permutations length {m} in Av({basis})")
+print(len(Av(basis_patterns).generate_cperms(m)))
 
 # Prints the number of Cayley permutations of size 0 to n in the class Av{basis_patterns}
 n = 7
+print(f"Number of Cayley permutations in the class Av({basis}):")
 print(Av(basis_patterns).counter(n))
 
 # Print True if the Cayley permutation 'cperm' avoids the basis
 cperm = CayleyPermutation([0, 1, 2, 3])
 print(
-    f"Is the Cayley permutation {cperm} in the class Av{tuple(basis_patterns)}:",
+    f"Is the Cayley permutation {cperm} in the class Av({basis}):",
     Av(basis_patterns).in_class(cperm),
 )
